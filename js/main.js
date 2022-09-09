941b540241fb2f4289e13a20430123df4b2c1845
Jonathan Romero <jonathandr600@gmail.com>
2022-09-09 14:14:13 -0300

    update js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,19 @@
+const init = () => {
+    burger_nav();
+};
+
 const ul = document.getElementById("ul");
 const body = document.querySelector("body");
 
 let burger_nav = () => {
     let menu = document.querySelector(".menu-burger");
-    let nav_list = document.querySelectorAll(".link-container li");
-    //
+
     menu.addEventListener("click", () => {
         val();
-        //
-        nav_list.forEach((link, i) => {
-            if (link.style.animation) {
-                link.style.animation = "";
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${
-                    i / 5 + 0.5
-                }s`;
-            }
-            //
-            menu.classList.toggle(`toggle`);
-        });
+        menu.classList.toggle(`toggle`);
     });
 
-    function val() {
+    const val = () => {
         if (ul.classList.contains("show-menu")) {
             ul.classList.remove("show-menu");
             body.classList.remove("no-move");
@@ -29,6 +21,6 @@ let burger_nav = () => {
             ul.classList.add("show-menu");
             body.classList.add("no-move");
         }
-    }
+    };
 };
-burger_nav();
+init();
