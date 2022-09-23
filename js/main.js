e53be8439a7c13fe718493faab3aa069871fa866
JonyWhalee <99540168+JonyWhalee@users.noreply.github.com>
2022-09-23 20:06:21 -0300

    home y nav links

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 const init = () => {
-    burger_nav();
+  burger_nav();
 };
 
 const ul = document.getElementById("ul");
 const body = document.querySelector("body");
 
 let burger_nav = () => {
-    let menu = document.querySelector(".menu-burger");
+  let menu = document.querySelector(".menu-burger");
 
-    menu.addEventListener("click", () => {
-        val();
-        menu.classList.toggle(`toggle`);
-    });
+  menu.addEventListener("click", () => {
+    val();
+    menu.classList.toggle(`toggle`);
+  });
 
-    const val = () => {
-        if (ul.classList.contains("show-menu")) {
-            ul.classList.remove("show-menu");
-            body.classList.remove("no-move");
-        } else {
-            ul.classList.add("show-menu");
-            body.classList.add("no-move");
-        }
-    };
+  const val = () => {
+    if (ul.classList.contains("show-menu")) {
+      ul.classList.remove("show-menu");
+      body.classList.remove("no-move");
+    } else {
+      ul.classList.add("show-menu");
+      body.classList.add("no-move");
+    }
+  };
 };
 init();
